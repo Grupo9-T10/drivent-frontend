17d537391c6c622e04c956134698cc5deca5748c
Lucas Neris <lucasnerism@gmail.com>
2023-08-03 04:59:58 -0300

    fix to import on paymentApi

--- src/services/paymentApi.js
+++ src/services/paymentApi.js
@@ -1,3 +1,5 @@
+import api from './api';
+
 export async function sendPay(ticketId, cardData) {
   const response = await api.post('/payments/process', { ticketId, cardData });
   return response.data;
